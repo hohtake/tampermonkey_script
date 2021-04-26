ed12305e54d9533aafcc3fa8e0f0393f165a27d6
hohtake <hohtake@opendoor.co.jp>
2021-04-26 10:36:46 +0900

    発動タイミング調整
     document ready時にMutationObserver設定するようにした

--- github/github-cybozu-to-remote2.user.js
+++ github/github-cybozu-to-remote2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         github-cybozu-to-remote2
 // @namespace    https://github.com/hohtake/tampermonkey_script
-// @version      0.1
+// @version      0.2
 // @description  cybozu url to remote2 url!
 // @author       hohtake@opendoor.co.jp
 // @homepage     https://github.com/hohtake/tampermonkey_script/
@@ -15,12 +15,23 @@
     const checkStartString = 'http://cybozu/';
     const modifyString = 'https://remote2.cybozu.co.jp/';
 
-    const targetLinks = document.getElementsByTagName('a');
-    for (let item of targetLinks) {
-        const hrefString = item.getAttribute('href');
-        if (hrefString != null && hrefString.startsWith(checkStartString)) {
-            item.setAttribute('href', hrefString.replace(checkStartString, modifyString));
+    const modifyLink = function() {
+        const targetLinks = document.getElementsByTagName('a');
+        for (let item of targetLinks) {
+            const hrefString = item.getAttribute('href');
+            if (hrefString != null && hrefString.startsWith(checkStartString)) {
+                item.setAttribute('href', hrefString.replace(checkStartString, modifyString));
+            }
         }
-    }
+    };
 
+    const checkTarget = document.getElementById('js-repo-pjax-container');
+    if (checkTarget) {
+    　　const observer = new MutationObserver((mutations) => {
+            modifyLink();
+        });
+
+　　    const observeOptions = {childList: true};
+    　　observer.observe(checkTarget, observeOptions);
+    }
 })();
--- github/github-remote2-to-cybozu.user.js
+++ github/github-remote2-to-cybozu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         github-remote2-to-cybozu
 // @namespace    https://github.com/hohtake/tampermonkey_script
-// @version      0.1
+// @version      0.2
 // @description  remote2 url to cybozu url!
 // @author       hohtake@opendoor.co.jp
 // @homepage     https://github.com/hohtake/tampermonkey_script/
@@ -15,12 +15,23 @@
     const checkStartString = 'https://remote2.cybozu.co.jp/';
     const modifyString = 'http://cybozu/';
 
-    const targetLinks = document.getElementsByTagName('a');
-    for (let item of targetLinks) {
-        const hrefString = item.getAttribute('href');
-        if (hrefString != null && hrefString.startsWith(checkStartString)) {
-            item.setAttribute('href', hrefString.replace(checkStartString, modifyString));
+    const modifyLink = function() {
+        const targetLinks = document.getElementsByTagName('a');
+        for (let item of targetLinks) {
+            const hrefString = item.getAttribute('href');
+            if (hrefString != null && hrefString.startsWith(checkStartString)) {
+                item.setAttribute('href', hrefString.replace(checkStartString, modifyString));
+            }
         }
-    }
+    };
 
+    const checkTarget = document.getElementById('js-repo-pjax-container');
+    if (checkTarget) {
+    　　const observer = new MutationObserver((mutations) => {
+            modifyLink();
+        });
+
+　　    const observeOptions = {childList: true};
+    　　observer.observe(checkTarget, observeOptions);
+    }
 })();
